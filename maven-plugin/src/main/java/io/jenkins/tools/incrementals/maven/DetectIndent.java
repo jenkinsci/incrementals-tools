ad99579ff53fdd3b938fcaae3620efdd8a953c77
Joseph Petersen <josephp90@gmail.com>
2018-07-10 02:18:49 +0200

    Basic indent detection

---
+++ maven-plugin/src/main/java/io/jenkins/tools/incrementals/maven/DetectIndent.java
@@ -0,0 +1,67 @@
+package io.jenkins.tools.incrementals.maven;
+
+import org.apache.commons.lang.StringUtils;
+
+public class DetectIndent {
+
+  public Indent detect(String input) {
+    if (StringUtils.isEmpty(input)) return new Indent();
+    else {
+      int size = 0;
+      char indent = ' ';
+      String[] inputs = input.split(System.getProperty("line.separator"));
+      for (String line: inputs) {
+        for (int i = 0; i < line.length(); i++) {
+          switch(line.charAt(i))
+          {
+            case '\t':
+              size++;
+              indent = '\t';
+              break;
+            case ' ':
+              indent = ' ';
+              size++;
+              break;
+            default:
+              if (size == 0) continue;
+              return new Indent(size, indent);
+          }
+        }
+      }
+    }
+    return new Indent();
+  }
+
+  public class Indent {
+    int size = 0;
+    char type = ' ';
+
+    public Indent() {
+    }
+
+    public Indent(int size, char indent) {
+      this.size = size;
+      this.type = indent;
+    }
+
+    public String getIndent() {
+      return StringUtils.repeat(String.valueOf(type), size);
+    }
+
+    public int getSize() {
+      return size;
+    }
+
+    public void setSize(int size) {
+      this.size = size;
+    }
+
+    public char getType() {
+      return type;
+    }
+
+    public void setType(char type) {
+      this.type = type;
+    }
+  }
+}
